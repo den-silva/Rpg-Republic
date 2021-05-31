2146359b6756df228fc540e25cc2fa306eb6a4a5
JFelipeAB <joao-alves-borges@hotmail.com>
2021-05-31 20:07:09 -0300

    funcionalidades

--- Views/content/adicional/sala.js
+++ Views/content/adicional/sala.js
@@ -3,7 +3,9 @@ var sala = function () {
         
     };
 
-    
+    var CriaNumeroSalas = function(){
+       return Math.floor((Math.random()*10) + 1); //cria salaaleatoria  
+    };
 
     return {
         controles: controles,
