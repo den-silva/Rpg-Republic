fa8d69b83787c0ef5fdb91d751089252a1de7da4
JFelipeAB <joao-alves-borges@hotmail.com>
2021-05-31 20:32:16 -0300

    validações

--- Views/content/adicional/sala.js
+++ Views/content/adicional/sala.js
@@ -1,13 +1,30 @@
 var sala = function () {
-    var controles = function () { 
-        
+
+    var controles = function () {
+        return {
+            DivChat: ".messages",
+            txtTexto: "#txtText",
+        };
     };
 
-    var CriaNumeroSalas = function(){
-       return Math.floor((Math.random()*10) + 1); //cria salaaleatoria  
+    var CriaNumeroSalas = function () {
+        return Math.floor((Math.random() * 10) + 1); //cria salaaleatoria  
+    };
+
+    var adicionarMensagem = function () {
+        debugger
+        $(".messages").append('<div ><strong>' + 'Usuario123' + '</strong>: ' + GetMensagem() + '</div><hr>')
+
+
+    };
+    var GetMensagem = function () {
+        var texto = $("#txtText").val();
+        $("#txtText").val('');
+        return texto;
     };
 
     return {
         controles: controles,
+        adicionarMensagem: adicionarMensagem
     };
 }();
